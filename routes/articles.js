34454fa19a948a7f9f3c399d7c56d1661e6e6242
Sam Ward-Pickering <samuel.wardpickering@gmail.com>
2022-04-01 17:50:04 +1300

    form input

--- routes/articles.js
+++ routes/articles.js
@@ -6,9 +6,21 @@ router.get('/new', (req, res) => {
     res.render('articles/new')
 })
 
+router.get('/:id', (req, res) => {
+
+})
+
 router.post('/', (req, res) => {
     const article = new Article({
-        
+        title: req.body.title,
+        description: req.body.description,
+        markdown: req.body.markdown
     })
+    try {
+        article = article.save()
+        res.redirect(`/articles/${article.id}`)
+    } catch (e) {
+        res.render('articles/new', { article: article })
+    }
 })
 module.exports = router
