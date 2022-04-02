9af0a6d1d3cb5ada61154bf79ae2ef9050a29c0e
Sam Ward-Pickering <samuel.wardpickering@gmail.com>
2022-04-02 17:01:31 +1300

    create show page for individual blog posts

--- server.js
+++ server.js
@@ -12,16 +12,7 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
-    const articles = [{
-        title: 'Test Article',
-        createdAt: new Date(),
-        description: 'Test Description'
-    },
-    {
-        title: 'Test Article 2',
-        createdAt: new Date(),
-        description: 'Test Description 2'
-    }]
+    
     res.render('articles/index', { articles: articles })
 })
 
