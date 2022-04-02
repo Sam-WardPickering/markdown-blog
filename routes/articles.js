030665281d0b00be955b6e547e90e8f9433e3625
Sam Ward-Pickering <samuel.wardpickering@gmail.com>
2022-04-02 16:38:03 +1300

    send post id in response

--- routes/articles.js
+++ routes/articles.js
@@ -7,11 +7,11 @@ router.get('/new', (req, res) => {
 })
 
 router.get('/:id', (req, res) => {
-
+    res.send(req.params.id)
 })
 
 router.post('/', async (req, res) => {
-    const article = new Article({
+    let article = new Article({
         title: req.body.title,
         description: req.body.description,
         markdown: req.body.markdown
@@ -20,6 +20,7 @@ router.post('/', async (req, res) => {
         article = await article.save()
         res.redirect(`/articles/${article.id}`)
     } catch (e) {
+        console.log(e)
         res.render('articles/new', { article: article })
     }
 })
