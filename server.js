c99e5ed22a27fa6cc75ecd521aa0e797e91cf008
Sam Ward-Pickering <samuel.wardpickering@gmail.com>
2022-04-03 20:31:50 +1200

    Debug and change port

--- server.js
+++ server.js
@@ -6,20 +6,18 @@ const methodOverride = require('method-override')
 const app = express()
 
 mongoose.connect('mongodb://localhost/blog', {
-    useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
 })
 
 app.set('view engine', 'ejs')
-
 app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride('_method'))
 
 app.get('/', async (req, res) => {
-    const articles = await Article.find().sort({
-        createdAt: 'desc' })
-    res.render('articles/index', { articles: articles })
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
 })
 
 app.use('/articles', articleRouter)
 
-app.listen(3000)
+app.listen(5000)
